c944be13121f70c029a52898ad966073952af611
memade <k34ubll5@outlook.com>
2023-01-16 09:47:25 +0800

    updated.

--- llvm/tools/clang/lib/Basic/Targets/X86.cpp
+++ llvm/tools/clang/lib/Basic/Targets/X86.cpp
@@ -1,4 +1,4 @@
-//===--- X86.cpp - Implement X86 target feature support -------------------===//
+﻿//===--- X86.cpp - Implement X86 target feature support -------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- llvm/tools/clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ llvm/tools/clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -1,4 +1,4 @@
-//===----- CGOpenMPRuntime.cpp - Interface to OpenMP Runtimes -------------===//
+﻿//===----- CGOpenMPRuntime.cpp - Interface to OpenMP Runtimes -------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
